16ad9eff1e7be0beb0fb71e71ba72b2b1281b7c7
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-04-05 11:04:55 -0600

    Adding Card class and starting Lists

--- src/main/java/com/detroitlabs/MuderMystery/model/Card.java
+++ src/main/java/com/detroitlabs/MuderMystery/model/Card.java
@@ -1,4 +1,37 @@
 package com.detroitlabs.MuderMystery.model;
 
 public class Card {
+    private String cardName;
+    private String identifier;    //w for weapon, p for people, L for location
+    private String picName;
+
+    public Card(String cardName, String identifier, String picName) {
+        this.cardName = cardName;
+        this.identifier = identifier;
+        this.picName = picName;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getPicName() {
+        return picName;
+    }
+
+    public void setPicName(String picName) {
+        this.picName = picName;
+    }
 }
