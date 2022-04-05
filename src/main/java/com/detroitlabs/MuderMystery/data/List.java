ce7d53fe44eff4b3b27c27de56eb516fc4977c83
Amanda Vollmar <amanda.vollmar@detroitlabs.com>
2022-04-05 12:37:48 -0600

    Add api and images

---
+++ src/main/java/com/detroitlabs/MuderMystery/data/List.java
@@ -0,0 +1,85 @@
+package com.detroitlabs.MuderMystery.data;
+
+public class List {
+    private Integer id;
+    private Integer order;
+    private Integer color;
+    private String name;
+    private Integer comment_count;
+    private Boolean shared;
+    private Boolean favorite;
+    private Integer sync_id;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    public Integer getColor() {
+        return color;
+    }
+
+    public void setColor(Integer color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getComment_count() {
+        return comment_count;
+    }
+
+    public void setComment_count(Integer comment_count) {
+        this.comment_count = comment_count;
+    }
+
+    public Boolean isShared() {
+        return shared;
+    }
+
+    public void setShared(Boolean shared) {
+        this.shared = shared;
+    }
+
+    public Boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public Integer getSync_id() {
+        return sync_id;
+    }
+
+    public void setSync_id(Integer sync_id) {
+        this.sync_id = sync_id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- src/main/java/com/detroitlabs/MuderMystery/service/ListService.java
+++ src/main/java/com/detroitlabs/MuderMystery/service/ListService.java
@@ -1,4 +1,15 @@
 package com.detroitlabs.MuderMystery.service;
 
+import com.detroitlabs.MuderMystery.data.List;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class ListService {
+    public static List fetchListData() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("https://api.todoist.com/rest/v1/projects/2289156017",
+                List.class);
+
+    }
 }
